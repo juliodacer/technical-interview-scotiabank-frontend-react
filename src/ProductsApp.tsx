b25a10c5e0697c6c6f1b406e032e438a9ffe880c
Julio Davila <juliodacer@gmail.com>
2026-02-12 08:46:25 -0500

    feat: tan stack query

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -1,6 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router";
 import { appRouter } from "./router/app.router";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      gcTime: 1000 * 60 * 10, // 10 minutes
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export const ProductsApp = () => {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={appRouter} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
