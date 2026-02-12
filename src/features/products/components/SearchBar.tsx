38afd1707fa750f5fdf5c06cee3d8f1d005bffe3
Julio Davila <juliodacer@gmail.com>
2026-02-12 08:03:50 -0500

    feat: debounce search bar

---
+++ src/features/products/components/SearchBar.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState, type KeyboardEvent } from "react";
+
+interface SearchBarProps {
+  placeholder: string;
+  onQuery: (query: string) => void;
+}
+
+export const SearchBar = ({
+  placeholder = "Buscar",
+  onQuery,
+}: SearchBarProps) => {
+  const [query, setQuery] = useState<string>("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onQuery(query);
+    }, 700);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [query, onQuery]);
+
+  const handleSearch = () => {
+    onQuery(query);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleSearch}>Buscar</button>
+    </div>
+  );
+};
--- src/features/products/pages/home/HomePage.tsx
+++ src/features/products/pages/home/HomePage.tsx
@@ -1,12 +1,19 @@
 import { CustomHeader } from "../../../../components/CustomHeader";
+import { SearchBar } from "../../components/SearchBar";
 
 export const HomePage = () => {
+  const handleSearch = (query: string) => {
+    console.log(query);
+  };
+
   return (
     <>
       <CustomHeader
         title="Gestión de Productos Bancarios"
         subtitle="Encuentra los productos financieros que necesitas"
       />
+
+      <SearchBar placeholder="Buscar producto" onQuery={handleSearch} />
     </>
   );
 };
